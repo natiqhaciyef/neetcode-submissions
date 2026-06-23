1914190888d4ef458e3f90acf0fcd0fe4fe19cd6
Natig Hajiyev <100693634+natiqhaciyef@users.noreply.github.com>
2026-06-23 16:31:19 +0400

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        var l = 0
+        var r = numbers.size - 1
+
+        while(l < r) {
+            val sum = numbers[l] + numbers[r]
+            if(sum > target) {
+                r -= 1
+            }else if(sum < target) {
+                l += 1
+            }else{
+                return intArrayOf(l + 1, r + 1)
+            }
+        }
+
+
+        return intArrayOf()
+    }
+}
