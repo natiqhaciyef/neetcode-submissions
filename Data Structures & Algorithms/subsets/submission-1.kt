faa954743e8676e36b99bccc3db2663d4d369760
Natig Hajiyev <100693634+natiqhaciyef@users.noreply.github.com>
2026-08-13 17:23:13 +0400

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.kt
@@ -0,0 +1,24 @@
+class Solution {
+    private val list = mutableSetOf<List<Int>>(listOf())
+
+    fun subsets(nums: IntArray): List<List<Int>> {
+        backtrack(nums.toList())
+        return list.toList()
+    }
+
+    private fun backtrack(arr: List<Int>) {
+        if(arr.size == 0) return
+        
+        val current = arr.toMutableList()
+        list.add(current)
+
+        val size = arr.size
+        for(i in 0 until size) {
+            val num = arr[i]
+
+            current.removeAt(i)
+            backtrack(current.toList())
+            current.add(i, num)
+        }
+    }
+}
